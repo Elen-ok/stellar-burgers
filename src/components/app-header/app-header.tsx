e6fe060ba78e389e32b2371c6007947219de55d3
Elena <lenchik.beautiful@gmail.com>
2026-05-13 12:34:50 +0300

    Restore working code from backup (8 мая 2026)

--- src/components/app-header/app-header.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-import { NavLink } from 'react-router-dom';
-import { AppHeaderUI } from '@ui';
-
-export const AppHeader: FC = () => {
-  return <AppHeaderUI userName="" />;
-};
